ec5b687dcf844fd96ec013b40d8b915b4b97678e
Violeta <holasoyviole@gmail.com>
2023-07-18 10:49:51 -0300

    avance

--- ecommerce-jess/src/App.jsx
+++ ecommerce-jess/src/App.jsx
@@ -1,9 +1,20 @@
+import { Provider } from "react-redux";
+import store from "../src/redux/store/store.js";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Home from './containers/Home';
+import Error404 from './containers/Error404'
+
 function App() {
 
   return (
-      <div className="text-blue-600">
-        app
-      </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          <Route path='*' element={<Error404/>}/>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </Router>
+    </Provider>
   )
 }
 
---
+++ ecommerce-jess/src/containers/Home.jsx
@@ -0,0 +1,11 @@
+import Layout from "../hooks/Layout";
+
+const Home = () => {
+  return (
+    <Layout>
+      <div className='text-blue-600'>Home</div>
+    </Layout>
+  );
+};
+
+export default Home;
